6e7af66b124de47b505ca5d04d200fb374237aa3
Björn Hutmacher <bjorn.hutmacher@gmail.com>
2015-01-25 14:48:40 +0100

    TWRP flags and fstab for shamu

--- device.mk
+++ device.mk
@@ -21,6 +21,9 @@
 
 
 PRODUCT_COPY_FILES += \
+    device/moto/shamu/twrp.fstab:recovery/root/etc/twrp.fstab
+
+PRODUCT_COPY_FILES += \
     device/moto/shamu/init.shamu.rc:root/init.shamu.rc \
     device/moto/shamu/init.shamu.power.rc:root/init.shamu.power.rc \
     device/moto/shamu/init.shamu.usb.rc:root/init.shamu.usb.rc \
